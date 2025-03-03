9754d60cbccfd28298ceadbdbf348cc336a8d28b
Hafidz <zuhayrshiddiq16@gmail.com>
2025-03-04 00:19:28 +0700

    berubah

--- TP_MOD_03/Form1.Designer.cs
+++ TP_MOD_03/Form1.Designer.cs
@@ -28,20 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(117, 98);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(198, 20);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(404, 83);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(273, 48);
+            this.btnSubmit.TabIndex = 1;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.Submit_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(338, 171);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(17, 13);
+            this.lblOutput.TabIndex = 2;
+            this.lblOutput.Text = "\"\"";
+            this.lblOutput.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(709, 402);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtInput);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label lblOutput;
     }
 }
 
--- TP_MOD_03/Form1.cs
+++ TP_MOD_03/Form1.cs
@@ -10,11 +10,36 @@ using System.Windows.Forms;
 
 namespace TP_MOD_03
 {
-    public partial class Form1: Form
+    public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string nama = txtInput.Text;
+            lblOutput.Text = $"Halo {nama}";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Submit_Click(object sender, EventArgs e)
+        {
+           
+            string nama = txtInput.Text;
+            lblOutput.Text = $"Halo {nama}";
+        
+
     }
+}
 }
